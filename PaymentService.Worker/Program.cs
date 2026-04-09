cc7100c595ee44989da9c9cd8e0d81c938f7895c
Uche Offiah <evaristusoffiah@gmail.com>
2026-04-08 22:23:08 -0400

    minor bug fix before test

--- PaymentService.Worker/Program.cs
+++ PaymentService.Worker/Program.cs
@@ -2,10 +2,13 @@ using PaymentService.Worker;
 using PaymentService.Worker.Messaging;
 using PaymentService.Worker.Repositories;
 using Microsoft.Extensions.Configuration;
+using PaymentService.Worker.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddHostedService<Worker>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("PaymentDb"));
 builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
 builder.Services.AddScoped<RabbitMqConsumer>();
 
